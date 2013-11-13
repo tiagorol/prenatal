dbd5099d2291dc339a7a5709a1fed3f2a594bb78
Jonath-ads <Jonath@edu.unifor.br>
2013-11-13 20:42:12 -0200

    Criação de tabelas.

--- prenatal/Script/PRENATAL.sql
+++ prenatal/Script/PRENATAL.sql
@@ -19,3 +19,25 @@ ALTER TABLE estado
   CACHE 1;
 ALTER TABLE estado_id_seq
   OWNER TO root;
+  
+  CREATE TABLE cidade
+(
+  id bigint NOT NULL,
+  nome character varying(255) NOT NULL,
+  estado_id bigint NOT NULL REFERENCES estado(id),
+  CONSTRAINT cidade_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cidade
+  OWNER TO root;
+
+CREATE SEQUENCE cidade_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE cidade_id_seq
+  OWNER TO root;  
