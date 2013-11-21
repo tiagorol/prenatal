9dbde1afacf213144676c46c5f70f711d010cb50
Jonath-ads <Jonath@edu.unifor.br>
2013-11-21 10:51:17 -0200

    ajuste

--- prenatal/src/main/java/br/com/prenatal/entity/enumeration/EstadoCivil.java
+++ prenatal/src/main/java/br/com/prenatal/entity/enumeration/EstadoCivil.java
@@ -5,9 +5,7 @@ public enum EstadoCivil {
 	
 	public String toString() {
 		switch (this) {
-		case SOLTEIRO:	
-			
-			
+		case SOLTEIRO:			
 			return "Solteiro";
 		case CASADO:			
 			return "Casado";
