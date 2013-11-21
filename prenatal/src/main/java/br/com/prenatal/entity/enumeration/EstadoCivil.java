e1df9b54054ede19b959953755462636c94c7f5f
Jonath-ads <Jonath@edu.unifor.br>
2013-11-21 10:42:29 -0200

    Listas do Enum

--- prenatal/src/main/java/br/com/prenatal/entity/enumeration/EstadoCivil.java
+++ prenatal/src/main/java/br/com/prenatal/entity/enumeration/EstadoCivil.java
@@ -2,4 +2,20 @@ package br.com.prenatal.entity.enumeration;
 
 public enum EstadoCivil {
 	SOLTEIRO, CASADO, DIVORCIADO, VIUVO;
+	
+	public String toString() {
+		switch (this) {
+		case SOLTEIRO:			
+			return "Solteiro";
+		case CASADO:			
+			return "Casado";
+		case DIVORCIADO:			
+			return "Divorciado";
+		case VIUVO:			
+			return "Viuvo";
+		default:
+			return null;
+		}
+	};
 }
+ 
