af382cbbac783ff11ca107b67bdc0cd6611696be
Jonath-ads <Jonath@edu.unifor.br>
2013-11-21 09:43:12 -0200

    Erro spring

---
+++ prenatal/src/main/java/br/com/prenatal/service/EstadosService.java
@@ -0,0 +1,28 @@
+package br.com.prenatal.service;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.prenatal.entity.Estado;
+
+@Service
+public class EstadosService {
+
+	@Autowired
+	public SessionFactory sessionFactory;
+
+	public Session getCurrentSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	public List<Estado> buscarTodos() {
+	
+		return getCurrentSession()
+				.createQuery("SELECT est FROM estado est").list();
+	}
+
+}
