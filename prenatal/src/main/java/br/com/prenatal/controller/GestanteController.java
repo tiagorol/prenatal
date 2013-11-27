4f716b44a5377f68f6d9f805da5f45769ee02702
TIAGO <TIAGO@TIAGO-PC>
2013-11-27 14:50:11 -0300

    Correção de bug na validação

--- prenatal/src/main/java/br/com/prenatal/controller/GestanteController.java
+++ prenatal/src/main/java/br/com/prenatal/controller/GestanteController.java
@@ -2,16 +2,12 @@ package br.com.prenatal.controller;
 
 import java.util.List;
 
-import javax.naming.Binding;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +20,6 @@ import br.com.prenatal.entity.enumeration.GrauEscolaridade;
 import br.com.prenatal.service.CidadesService;
 import br.com.prenatal.service.EstadosService;
 import br.com.prenatal.service.GestanteService;
-import br.com.prenatal.validator.UserValidator;
 
 @Controller
 @RequestMapping("/gestante/")
@@ -39,15 +34,6 @@ public class GestanteController {
 	@Autowired
 	private CidadesService cidadeService;
 	
-	@Autowired
-	private UserValidator userValidator;
-	
-	@InitBinder
-    protected void initBinder(HttpServletRequest request,
-                              ServletRequestDataBinder binder) throws Exception {
-        binder.setValidator(userValidator);
-    }
-	
 	@RequestMapping(value = "prepararInserir", method = RequestMethod.GET)
 	public String salvar(ModelMap model) {
 				
@@ -59,7 +45,7 @@ public class GestanteController {
 	public String salvar(@Valid Gestante gestante, BindingResult result, ModelMap model) {
 		
 		if (result.hasFieldErrors()) {
-			return "gestante/gestanteForm";		
+			return "gestante/gestanteForm";
 		}
 		System.out.println(gestante + "Cheguei aqui..");
 		model.addAttribute("message", "Success");
@@ -72,14 +58,17 @@ public class GestanteController {
 	public EstadoCivil[] listaEstadoCivil(){
 		return EstadoCivil.values();
 	}
+	
 	@ModelAttribute("listaGrauEscolaridade")
 	public GrauEscolaridade[] listaGrauEscolaridades(){
 		return GrauEscolaridade.values();
 	}
+	
 	@ModelAttribute("listaCidade")
 	public List<Cidade> listaCidade(){
 		return cidadeService.buscarTodas();
 	}
+	
 	@ModelAttribute("listaEstado")
 	public List<Estado> listaEstados(){
 		return estadoService.buscarTodos();
